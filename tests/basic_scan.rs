7e36959fc8269fc3da59c0b52c6b45884ca4a9f7
HuRuilizhen <huruilizhen@gmail.com>
2025-09-17 15:40:44 +0800

    #1 feat(test): add integration test

---
+++ tests/basic_scan.rs
@@ -0,0 +1,26 @@
+#[test]
+fn test_single_scan_google() {
+    let output = std::process::Command::new("cargo")
+        .args(["run", "--", "--target", "google.com", "--ports", "80"])
+        .output()
+        .unwrap();
+    assert!(output.status.success());
+}
+
+#[test]
+fn test_scan_localhost() {
+    let output = std::process::Command::new("cargo")
+        .args(["run", "--", "--target", "127.0.0.1", "--ports", "80"])
+        .output()
+        .unwrap();
+    assert!(output.status.success());
+}
+
+#[test]
+fn test_scan_invalid_ip() {
+    let output = std::process::Command::new("cargo")
+        .args(["run", "--", "--target", "256.256.256.256", "--ports", "80"])
+        .output()
+        .unwrap();
+    assert!(!output.status.success());
+}
