26aadaa5dd58c300316e12d1296c5c9e444282d0
Ruilizhen Hu <huruilizhen@gmail.com>
2025-09-19 03:05:25 +0800

    feat(uiux) 20250919 (#12)
    * #11 feat(uiux): enable display service name
    
    * #11 refactor(upshot): clean code

--- src/upshot/mod.rs
+++ src/upshot/mod.rs
@@ -5,9 +5,33 @@
 
 use crate::cli::{DisplayConfig, DisplayFormat};
 use colored::Colorize;
+use phf::phf_map;
 use serde::Serialize;
 use std::fmt;
 use std::io;
+
+static SERVICE_MAP: phf::Map<u16, &'static str> = phf_map! {
+    20u16 | 21u16 => "FTP",
+    989u16 | 990u16 => "FTPS",
+    25u16 => "SMTP",
+    465u16 => "SMTPS",
+    80u16 => "HTTP",
+    443u16 => "HTTPS",
+    110u16 => "POP3",
+    995u16 => "POP3S",
+    119u16 => "NNTP",
+    563u16 => "NNTPS",
+    143u16 => "IMAP",
+    993u16 => "IMAPS",
+    22u16 => "SSH",
+    23u16 => "TELNET",
+    53u16 => "DNS",
+    123u16 => "NTP",
+};
+
+fn parse_service(port: u16) -> String {
+    SERVICE_MAP.get(&port).unwrap_or(&"UNK").to_string()
+}
 
 #[derive(Debug, Clone, Serialize)]
 pub enum Status {
@@ -18,8 +42,8 @@ pub enum Status {
 impl fmt::Display for Status {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = match self {
-            Status::OPEN => "OPEN".green().bold().to_string(),
-            Status::CLOSE => "CLOSE".red().bold().to_string(),
+            Status::OPEN => "OPEN".green().bold(),
+            Status::CLOSE => "CLOSE".red().bold(),
         };
         write!(f, "{}", s)
     }
@@ -30,20 +54,33 @@ pub struct Upshot {
     pub target: String,
     pub ip: String,
     pub port: u16,
+    pub service: String,
     pub status: Status,
+}
+
+impl Upshot {
+    pub fn new(target: String, ip: String, port: u16, status: Status) -> Self {
+        Upshot {
+            target: target,
+            ip: ip,
+            port: port,
+            service: parse_service(port),
+            status: status,
+        }
+    }
 }
 
 pub fn upshot_normal(upshot: Upshot) -> String {
     format!(
-        "{}\t{}:{}\t{}\n",
-        upshot.target, upshot.ip, upshot.port, upshot.status,
+        "{}\t{}:{}\t{}\t{}\n",
+        upshot.target, upshot.ip, upshot.port, upshot.status, upshot.service
     )
 }
 
 pub fn upshot_quiet(upshot: Upshot) -> String {
     match upshot.status {
-        Status::OPEN => return format!("{}\n", upshot.port),
-        Status::CLOSE => return "".to_string(),
+        Status::OPEN => format!("{}\n", upshot.port),
+        Status::CLOSE => String::new(),
     }
 }
 
